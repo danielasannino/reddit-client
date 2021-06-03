0c3881a1da3231371853ed705abadb7085a158ff
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-03 19:35:59 -0400

    added material ui grid

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Counter } from './features/reddit/Counter';
 import { Header } from './features/Header/Header';
 import { PostList } from './features/PostList/PostList';
+import { Grid } from '@material-ui/core';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <PostList />
-        <Counter />
-      </header>
+    <div className='App'>
+      <Grid container style={{ background: 'lightgray' }}>
+        <Grid item xs={12} style={{ background: 'gray' }}>
+          <Header />
+        </Grid>
+        <Grid item xs={6}>
+          <PostList />
+        </Grid>
+        <Grid item xs={12}>
+          <Counter />
+        </Grid>
+      </Grid>
     </div>
   );
 }
