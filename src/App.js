7877516e5650504aa5c3b821128f041b47999b17
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-03 19:46:02 -0400

    added Paper from material-ui and removed counter from app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Counter } from './features/reddit/Counter';
 import { Header } from './features/Header/Header';
 import { PostList } from './features/PostList/PostList';
 import { Grid } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 
 function App() {
 
   return (
     <div className='App'>
-      <Grid container style={{ background: 'lightgray' }}>
-        <Grid item xs={12} style={{ background: 'gray' }}>
-          <Header />
+      <Paper>
+        <Grid container>
+          <Grid item xs={12}>
+            <Header />
+          </Grid>
+          <Grid item xs={8} align-items='center'>
+            <PostList />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <PostList />
-        </Grid>
-        <Grid item xs={12}>
-          <Counter />
-        </Grid>
-      </Grid>
+      </Paper>
     </div>
   );
 }
