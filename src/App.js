6f4a86fcb0f16bb1e554745e4dfcf76455bd30a5
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-04 14:17:59 -0400

    added margin around post cards

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import { Header } from './features/Header/Header';
-import { PostList } from './features/PostList/PostList';
-import { Grid } from '@material-ui/core';
-import { Paper } from '@material-ui/core';
-import { CategoriesList } from './features/CategoriesList/CategoriesList';
-
+import React from 'react'
+import { CategoriesList } from './features/CategoriesList/CategoriesList'
+import { Header } from './features/Header/Header'
+import { PostList } from './features/PostList/PostList'
+import { Grid } from '@material-ui/core'
+import { Paper } from '@material-ui/core'
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       <Paper>
         <Grid container>
           <Grid item xs={12}>
             <Header />
           </Grid>
-          <Grid item xs={8} align='center'>
+          <Grid item xs={2}>
+          </Grid>
+          <Grid item xs={6} align="center">
             <PostList />
           </Grid>
-          <Grid item xs={4} align='center'>
+          <Grid item xs={2} align="center">
             <CategoriesList />
+          </Grid>
+          <Grid item xs={2}>
           </Grid>
         </Grid>
       </Paper>
     </div>
   );
 }
-
 export default App;
