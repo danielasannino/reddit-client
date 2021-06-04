ef5280b312e42b82376adf24e11ec37c33ecf97a
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-04 19:42:00 -0400

    added message count view and icon

--- src/features/posts/DiscussonCard.js
+++ src/features/posts/DiscussonCard.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
 import Card from '@material-ui/core/Card';
 import { TimeAgo } from './TimeAgo'
+import SwapVertOutlinedIcon from '@material-ui/icons/SwapVertOutlined';
+import { Grid } from '@material-ui/core'
 
 const useStyles = makeStyles({
     root: {
@@ -13,25 +14,28 @@ const useStyles = makeStyles({
         maxWidth: 140
     },
 });
-
-export const DiscussionCard = props => {
+export const DiscussionCard = (props) => {
     const { post } = props;
-    const classes = useStyles();
-
+    const classes = useStyles()
     return (
         <Card className={classes.root}>
             <CardContent>
-                <Typography variant='body1' align='left'>
-                    {post.ups}
-                </Typography>
-                <Typography variant='body1' color='textSecondary'>
-                    Posted by: {post.author}
+                <Grid container direction="row" alignItems="center">
+                    <SwapVertOutlinedIcon fontSize="small" />
+                    <Typography variant="caption" align="left">
+                        {post.ups}
+                    </Typography>
+                </Grid>
+
+                <Typography variant="body1" color="textSecondary">
+                    Posted by {post.author}
                     <TimeAgo timestamp={post.created_utc} />
                 </Typography>
-                <Typography variant='body1' color='textPrimary'>
+
+                <Typography variant="body1" color="textPrimary">
                     {post.body}
                 </Typography>
             </CardContent>
         </Card>
-    );
-};
+    )
+}
--- src/features/posts/PostCard.js
+++ src/features/posts/PostCard.js
@@ -5,28 +5,27 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
-import { TimeAgo } from './TimeAgo';
+import { Link } from 'react-router-dom'
+import { TimeAgo } from './TimeAgo'
+import SwapVertOutlinedIcon from '@material-ui/icons/SwapVertOutlined';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
         margin: 10
-    },
-    media: {
-        maxWidth: 140
     }
 });
 
-export const PostCard = props => {
+export const PostCard = (props) => {
     const { post } = props;
-    const classes = useStyles();
-
+    const classes = useStyles()
     return (
         <Card className={classes.root}>
             <CardActionArea component={Link} to={`/discussion/${post.subreddit}/${post.id}`} onClick={() => window.scrollTo(0, 0)}>
                 <CardMedia
-                    component='img'
-                    alt=''
+                    component="img"
+                    alt=""
                     image={post.imgUrl}
                     title={post.title}
                 />
@@ -38,12 +37,17 @@ export const PostCard = props => {
                     image={post.videoUrl}
                     title={post.title}
                 />}
+
                 <CardContent>
-                    <Typography variant='body1' align='left'>
-                        {post.ups}
-                    </Typography>
-                    <Typography variant='body1' color='textSecondary'>
-                        Posted by: {post.author}
+                    <Grid container direction="row" alignItems="center">
+                        <SwapVertOutlinedIcon fontSize="small" />
+                        <Typography variant="caption" align="left">
+                            {post.ups}
+                        </Typography>
+                    </Grid>
+
+                    <Typography variant="body1" color="textSecondary">
+                        Posted by {post.author}
                         <TimeAgo timestamp={post.created_utc} />
                     </Typography>
                     <Typography variant="h6" color="textPrimary">
@@ -52,8 +56,15 @@ export const PostCard = props => {
                     <Typography>
                         {`r/${post.subreddit}`}
                     </Typography>
+
+                    <Grid container direction="row" alignItems="center">
+                        <ChatBubbleOutlineIcon fontSize="small" />
+                        <Typography variant="caption" align="left">
+                            {post.num_comments}
+                        </Typography>
+                    </Grid>
                 </CardContent>
             </CardActionArea>
         </Card>
-    );
-};
+    )
+}
